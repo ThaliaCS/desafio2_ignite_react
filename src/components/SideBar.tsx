d06f664d0f89d1dd005227324c400bb9fbb62e35
ThaliaCS <thalia.schone@gmail.com>
2021-07-10 15:34:33 -0300

    completed challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,45 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from '../components/Button';
+
+
+interface GenreResponseProps {
+  
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+  
+}
+
+interface SidebarProps {
+
+  genres: GenreResponseProps[];
+  setSelectedGenreId: React.Dispatch<React.SetStateAction<number>>;
+  selectedGenreId: number;
+}
+
+
+export function SideBar(props: SidebarProps) {
+
+  function handleClickButton(id: number) {
+    props.setSelectedGenreId(id);
+  }
+  
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
